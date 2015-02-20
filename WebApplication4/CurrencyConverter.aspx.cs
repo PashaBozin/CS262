1bfc677badcbd7c67c93737835731832ead7287b
PashaBozin <pbozin22@bigbend.edu>
2015-02-19 17:57:45 -0800

    currency converter

---
+++ WebApplication4/CurrencyConverter.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CurrencyConverter : System.Web.UI.Page
+{
+    protected void Convert_ServerClick(object sender, EventArgs e)
+    {
+        decimal USAmount = Decimal.Parse(US.Value);
+        decimal euroAmount = USAmount * (Decimal)0.85;
+        Result.InnerText = USAmount.ToString() + "U.S. dollars=";
+        Result.InnerText += euroAmount.ToString() + "Euros.";
+    }
+}
